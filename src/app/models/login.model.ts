16308af1d493cd4f248a350cf18d72815f770bb1
Enzzo Cansani <enzzocansani3@gmail.com>
2024-09-11 20:07:01 -0700

    supplier or client

--- src/app/models/login.model.ts
+++ src/app/models/login.model.ts
@@ -1,7 +1,16 @@
-export interface Credentials {
-  email: string
-  password: string
+export interface ClientCredentials {
+  isClient: true; 
+  cpf: string;
+  password: string;
 }
+
+export interface SupplierCredentials {
+  isClient: false; 
+  cnpj: string;
+  password: string;
+}
+
+export type Credentials = ClientCredentials | SupplierCredentials;
 
 export interface LoginResponse {
   jwt: string
