39859f2ea4faf63048e26a45a64ea1a522261c39
Enzzo Cansani <enzzocansani3@gmail.com>
2024-10-15 21:28:41 -0700

    add home page (everything static/no components)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ProductsComponent } from './components/products/products.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { HomeComponent } from './components/home/home.component';
 import { authorizationGuard } from './_guard/authorization.guard';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent, /* canActivate: [authorizationGuard] */ },
   { path: 'register', component: SignUpComponent },
   { path: 'login', component: SignInComponent },
   { path: 'products', component: ProductsComponent, /* canActivate: [authorizationGuard] */ },
