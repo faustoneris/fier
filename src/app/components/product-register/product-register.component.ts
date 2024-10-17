14c16c71506a62613bb59e98756fd8340dc4a0aa
Igor Sousa <igor2001silva3@gmail.com>
2024-10-17 13:07:21 -0300

    Adicionando form

--- src/app/components/product-register/product-register.component.ts
+++ src/app/components/product-register/product-register.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-register',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ReactiveFormsModule],
   templateUrl: './product-register.component.html',
   styleUrl: './product-register.component.css'
 })
-export class ProductRegisterComponent {
 
+export class ProductRegisterComponent{
+  formProductRegister: FormGroup;
+
+  constructor() {
+    this.formProductRegister = new FormGroup({
+      productName: new FormControl(''),
+      productPrice: new FormControl(''),
+      productCategory: new FormControl(''),
+      productDescription: new FormControl(''),
+      productSpecification: new FormControl(''),
+      productDocument: new FormControl(''),
+      productImage: new FormControl('')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formProductRegister.value);
+  }
 }
