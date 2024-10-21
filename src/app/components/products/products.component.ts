c12fd1b1b6b61942c9860c540f464e6cc3e78d55
Igor Sousa <igor2001silva3@gmail.com>
2024-10-21 16:33:39 -0300

    Adicionando form na tela de produto

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from '../footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule, RouterModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  bidMade = false;
 
+  bidInfos = {
+    bidAmount: '',
+    qtdProduct: ''
+  }
+
+  onBid(){
+    console.log("Bid button clicked");
+  }
 }
