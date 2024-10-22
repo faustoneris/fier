fee44120e45a639c6b70d68a3103a8ce23bc1bfd
Enzzo Cansani <enzzocansani3@gmail.com>
2024-10-21 21:29:48 -0700

    add search

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  @Output() searchChanged = new EventEmitter<string>()
 
+  onSearchChange(event: any): void {
+    const searchQuery = (event.target as HTMLInputElement).value
+    this.searchChanged.emit(searchQuery)
+  }
 }
