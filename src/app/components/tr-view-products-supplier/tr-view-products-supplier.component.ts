117fba29cec86f6cbb31a664ccdd20d002b6052a
Igor Sousa <igor2001silva3@gmail.com>
2024-10-22 23:46:18 -0300

    Criando linha da tabela

---
+++ src/app/components/tr-view-products-supplier/tr-view-products-supplier.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tr-view-products-supplier',
+  standalone: true,
+  imports: [],
+  templateUrl: './tr-view-products-supplier.component.html',
+  styleUrl: './tr-view-products-supplier.component.css'
+})
+export class TrViewProductsSupplierComponent {
+
+}
--- src/app/components/view-products-supplier/view-products-supplier.component.ts
+++ src/app/components/view-products-supplier/view-products-supplier.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { TrViewProductsSupplierComponent } from '../tr-view-products-supplier/tr-view-products-supplier.component';
 
 @Component({
   selector: 'app-view-products-supplier',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, TrViewProductsSupplierComponent],
   templateUrl: './view-products-supplier.component.html',
   styleUrl: './view-products-supplier.component.css'
 })
